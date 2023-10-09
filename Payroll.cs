bb3dd8a68ed5596c94b10b38b00dd130028b0851
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-10-09 13:44:49 +0530

    [Dhanya R Hegde] : Added Emloyee Details with Threading

---
+++ Payroll.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadingDemo
+{
+    public class Payroll
+    {
+        public int EmployeeID { get; set; }
+        
+        public string EmployeeName { get; set; }    
+
+        public int EmployeeAge { get; set; }
+
+        public int EmployeeSalary { get; set; }
+
+        public Payroll(int EmployeeID, string EmployeeName, int EmployeeAge, int EmployeeSalary)
+        {
+            this.EmployeeID = EmployeeID;
+            this.EmployeeName = EmployeeName;
+            this.EmployeeAge = EmployeeAge;
+            this.EmployeeSalary = EmployeeSalary;
+        }
+
+    }
+}
---
+++ PayrollOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadingDemo
+{
+    public class PayrollOperations
+    {
+        List<Payroll> payrollObj1 = new List<Payroll>();
+
+        public void addEmployee(List<Payroll> payrollObj1)
+        {
+            payrollObj1.ForEach(payrollData =>
+            {
+                Console.WriteLine("Employee being added : " + payrollData.EmployeeName);
+                this.addEmployeeToList(payrollData);
+                Console.WriteLine("Employee added : " + payrollData.EmployeeName);
+            });
+        }
+
+        public void addEmployeeWithThread(List<Payroll> payrollObj1)
+        {
+            payrollObj1.ForEach(payrollData =>
+            {
+                Task Thread = new Task(() =>
+                {
+                    Console.WriteLine("Employee being added : " + payrollData.EmployeeName);
+                    this.addEmployeeToList(payrollData);
+                    Console.WriteLine("Employee added : " + payrollData.EmployeeName);
+                });
+                Thread.Start();
+            });
+        }
+
+        public void addEmployeeToList(Payroll payroll)
+        {
+            payrollObj1.Add(payroll);
+        }
+    }
+}
